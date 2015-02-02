f2bf062ee2eac46acd95e03994f78cd68f2a12b4
Rajendra Ranjit <raj@Ranjit.local>
2015-02-02 21:58:59 +0000

    This is first commit to add all the required files. y

---
+++ public/a.php
@@ -0,0 +1,3 @@
+this is where you should be while you are learning this language. 
+this is when it shoudl be done and how it should be done. if the system should accept the way it should be doing the job in the first palce. 
+and if you do not like the it is working now then it is upto you how you want to be done and i am not going to interfare with this anymore
